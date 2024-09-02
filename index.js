1fb618fd67f398a60b9653e64e72991a430ef08b
Sumit1209 <sumitmintu12@gmail.com>
2024-09-03 00:56:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/*function changeBg(){
+    var navnar1=document.getElementById("nav")
+    var scrollValue=window.scrollY;
+  console.log(scrollValue);
+  if (scrollValue<630){
+    navnar1.classList.remove('bg')
+  }
+  else{
+    navnar1.classList.add('bg')
+  }
+
+  }
+  window.addEventListener('scroll',changeBg)
+
+*/
+ 
